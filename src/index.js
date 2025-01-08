48f7dc801f915f699cb9951115208c58ccb313fc
ismakjn <ismakjn@gmail.com>
2025-01-08 09:49:59 +0200

    Restoring

--- src/index.js
+++ src/index.js
@@ -16,5 +16,3 @@ app.listen(process.env.PORT || 3000, 'localhost', () => {
     console.log('Listening on port ' + process.env.PORT || 3000);
     console.log(process.env.GOOGLE_PLACE_ENDPOINT)
 })
-
-// test
